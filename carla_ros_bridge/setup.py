0afe6e1a1146f70c04dd8660d70d236c8a887683
Ubuntu <ubuntu@carla-ros2-erdo.lxd>
2025-12-13 23:12:37 +0000

    new launch with segmentation trial

--- carla_ros_bridge/setup.py
+++ carla_ros_bridge/setup.py
@@ -33,7 +33,8 @@ elif ROS_VERSION == 2:
         license='MIT',
         tests_require=['pytest'],
         entry_points={
-            'console_scripts': ['bridge = carla_ros_bridge.bridge:main'],
+            'console_scripts': ['bridge = carla_ros_bridge.bridge:main',
+            'seg_node = carla_ros_bridge.carla_seg_node:main'],
         },
         package_dir={'': 'src'},
         package_data={'': ['CARLA_VERSION']},
